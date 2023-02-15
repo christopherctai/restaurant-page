d097121d595070b7bcbb7a6d5a67c3a94fb70247
Chris Tai <christopherctai@gmail.com>
2023-02-15 09:04:43 -0800

    Finish adding headers for Menu and Contact pages

--- src/goContact.js
+++ src/goContact.js
@@ -1,8 +1,13 @@
 export function goContact(mainContent) {
     mainContent.textContent = "";
+
+    let heading = document.createElement('div');
+    heading.classList.add('heading');
+    heading.textContent = "Gas Frang";
+    mainContent.append(heading);
     
     let gusFringImage = document.createElement('img');
-    gusFringImage.src = '/src/images/gus-fring.jpg';
+    gusFringImage.src = '/src/images/gus-fring.jpg'; 
     gusFringImage.classList.add('los-pollos-image');
     gusFringImage.alt = 'Gus Fring';
     gusFringImage.title = 'Copyright Wikimedia Commons';
