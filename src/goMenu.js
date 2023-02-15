45d8b2c33e68f70229ca1ca0323edb1ff9ca925d
Chris Tai <christopherctai@gmail.com>
2023-02-15 08:55:45 -0800

    Add new objects for menu items

--- src/goMenu.js
+++ src/goMenu.js
@@ -2,7 +2,9 @@ export function goMenu(mainContent) {
     mainContent.textContent = "";
     
     const menuItemPictures = [
-        {imagePath: '/src/images/fried-chicken.jpg', description: "FRIED CHICKEN: What other kind of meal would satisfy your cravings besides our world famous fried chicken? Taste the delicious crunch and you'll never go back to normal food again"}
+        {imagePath: '/src/images/fried-chicken.jpg', description: "FRIED CHICKEN: What other kind of meal would satisfy your cravings besides our world famous fried chicken? Taste the delicious crunch and you'll never go back to normal food again"},
+        {imagePath: '/src/images/coleslaw.jpeg', description: "COLESLAW: Enjoy the delicious crunch of slimy coleslaw, the only 'healthy' item on the menu! It'll make you feel better about the terrible food you're eating!"},
+        {imagePath: '/src/images/toast.jpg', description: "TOAST: You've never had toast like this before. So good, it tastes like crack"}
     ]
 
     console.log(menuItemPictures[0])
